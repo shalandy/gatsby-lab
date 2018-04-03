1869611f4c913c78c011301f5e291264c04a0834
shalandy <shalandy@berkeley.edu>
2018-04-02 18:31:42 -0700

    removed sheets plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
   'gatsby-plugin-react-helmet',
   // add this object:
+  /*
   {
       resolve: 'gatsby-source-google-sheets',
       options: {
@@ -12,7 +13,7 @@ module.exports = {
           worksheetTitle: 'mystery',
           //credentials: require("./InnoD\ Mystery-975d82e0c439.json")
       },
-  },
+  },*/
   {
     resolve: `gatsby-source-contentful`,
     options: {
